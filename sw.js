a9d2a9c4d7371dc8a67abdb37c8fb3dcffa4d3b3
pravoslavljekalendar <bbebiron@gmail.com>
2026-03-22 16:51:49 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+javascript
+const cacheName = 'pravoslavlje-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './ikona.png',
+  './papir.png'
+];
+
+// Instalacija i čuvanje fajlova
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+// Otvaranje fajlova čak i kad nema interneta
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(rec => {
+      return rec || fetch(evt.request);
+    })
+  );
+});
