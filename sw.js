c1669fea0e1b874252ec0b74e3317ef3d116f203
pravoslavljekalendar <bbebiron@gmail.com>
2026-04-26 00:47:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const cacheName = 'pravoslavlje-v2';
+const cacheName = 'pravoslavlje-v3'; // Promenili smo verziju na v3 da bi telefon znao da treba ponovo da učita
 const assets = [
   './',
   './index.html',
@@ -10,12 +10,37 @@ const assets = [
   './papir.png'
 ];
 
-// Instalacija i čuvanje fajlova
+// Instalacija i čuvanje fajlova u keš
 self.addEventListener('install', evt => {
   evt.waitUntil(
     caches.open(cacheName).then(cache => {
-      console.log('Skladištenje fajlova u keš...');
+      console.log('Skladištenje fajlova za offline rad...');
       return cache.addAll(assets);
+    })
+  );
+});
+
+// Aktivacija i brisanje starog keša
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== cacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Ključni deo: Omogućava da se sajt otvori bez interneta
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request).catch(() => {
+        if (evt.request.url.indexOf('.html') > -1) {
+          return caches.match('./index.html');
+        }
+      });
     })
   );
 });
