537bcad42ef7b41c32b8293ba68898ac74541711
pravoslavljekalendar <bbebiron@gmail.com>
2026-04-26 11:02:17 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,28 @@
 
-const cacheName = 'pravoslavlje-v3'; // Promenili smo verziju na v3 da bi telefon znao da treba ponovo da učita
+const cacheName = 'pravoslavlje-v4'; // Povećali smo na v4 da telefon shvati promenu
 const assets = [
   './',
   './index.html',
   './manifest.json',
+  './svetitelji.json',
   './ikona.png',
   './ikona192.png',
   './ikona512.png',
   './papir.png'
 ];
 
-// Instalacija i čuvanje fajlova u keš
+// 1. Instalacija: Odmah preuzmi kontrolu i snimi osnovne fajlove
 self.addEventListener('install', evt => {
+  self.skipWaiting(); 
   evt.waitUntil(
     caches.open(cacheName).then(cache => {
-      console.log('Skladištenje fajlova za offline rad...');
+      console.log('Skladištenje osnovnih fajlova...');
       return cache.addAll(assets);
     })
   );
 });
 
-// Aktivacija i brisanje starog keša
+// 2. Aktivacija: Obriši stare verzije (v1, v2, v3) da ne troše prostor
 self.addEventListener('activate', evt => {
   evt.waitUntil(
     caches.keys().then(keys => {
@@ -32,24 +34,24 @@ self.addEventListener('activate', evt => {
   );
 });
 
-// Ključni deo: Omogućava da se sajt otvori bez interneta
+// 3. Glavna magija: "Usisivač" koji pamti mesece i Prologe za offline rad
 self.addEventListener('fetch', evt => {
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request).catch(() => {
-        if (evt.request.url.indexOf('.html') > -1) {
-          return caches.match('./index.html');
-        }
+      // Ako već imamo u memoriji (offline), vrati to.
+      // Ako nemamo, idi na internet i usput snimi taj fajl za sledeći put!
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(cacheName).then(cache => {
+          // Snimamo fajl (npr. maj-2026.html) u keš memoriju
+          cache.put(evt.request.url, fetchRes.clone());
+          return fetchRes;
+        });
       });
-    })
-  );
-});
-
-// Otvaranje fajlova čak i kad nema interneta
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(rec => {
-      return rec || fetch(evt.request);
+    }).catch(() => {
+      // Ako baš nema ni interneta ni tog fajla u memoriji, prikaži početnu
+      if (evt.request.url.indexOf('.html') > -1) {
+        return caches.match('./index.html');
+      }
     })
   );
 });
