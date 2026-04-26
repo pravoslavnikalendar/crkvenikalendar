5cf5334d915f296e6e263d168423ca15c82d7011
pravoslavljekalendar <bbebiron@gmail.com>
2026-04-26 12:44:55 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -77,8 +77,9 @@ self.addEventListener('fetch', evt => {
           cache.put(evt.request.url, fetchRes.clone());
           return fetchRes;
         });
-      });
+  });
     }).catch(() => {
+      // Ako baš nema ni interneta ni tog fajla u memoriji, prikaži početnu
       if (evt.request.url.indexOf('.html') > -1) {
         return caches.match('./index.html');
       }
