febd00b1c8e172e38b807889fc3354ea1c769af4
pravoslavljekalendar <bbebiron@gmail.com>
2026-05-19 12:41:12 +0200

    Update service worker for caching and fetch handling

--- sw.js
+++ sw.js
@@ -1,82 +1,180 @@
-const cacheName = 'pravoslavna-riznica-final-v57';
+const cacheName = 'pravoslavna-riznica-final-v50';
 const assets = [
-  './',
+  '/',
   './index.html',
-  './molitvenik.html',
   './psaltir.html',
-  './kalendar.html',
-  './prolog.html',
-  './novizavet.html',
+  './molitvenik.html',
+  './novi-zavet.html',
+  './ohridski-prolog.html',
   './duhovne-pouke.html',
   './duhovne-poucne-price.html',
   './praznici.html',
   './post-i-trapave-sedmice.html',
   './vaskrsnji-kalendar.html',
-  './stil.css',
-  './skripta.js',
+  './januar-2026.html',
+  './februar-2026.html',
+  './mart-2026.html',
+  './april-2026.html',
+  './maj-2026.html',
+  './jun-2026.html',
+  './jul-2026.html',
+  './avgust-2026.html',
+  './septembar-2026.html',
+  './oktobar-2026.html',
+  './novembar-2026.html',
+  './decembar-2026.html',
+  './prolog-januar.html',
+  './prolog-februar.html',
+  './prolog-mart.html',
+  './prolog-april.html',
+  './prolog-maj.html',
+  './prolog-jun.html',
+  './prolog-jul.html',
+  './prolog-avgust.html',
+  './prolog-septembar.html',
+  './prolog-oktobar.html',
+  './prolog-novembar.html',
+  './prolog-decembar.html',
+  './zitije-bozic.html',
+  './zitije-arandjelovdan.html',
+  './zitije-blagovesti.html',
+  './zitije-bogojavljenje.html',
+  './zitije-cveti.html',
+  './zitije-djurdjevdan.html',
+  './zitije-duhovski-ponedeljak.html',
+  './zitije-duhovski-utorak.html',
+  './zitije-ivanjdan.html',
+  './zitije-jovankrstitelj.html',
+  './zitije-kirilo-metodije.html',
+  './zitije-konstantin-jelena.html', 
+  './zitije-krstovdan.html',
+  './zitije-mala-gospojina.html',
+  './zitije-mitrovdan.html',
+  './zitije-nikoljdan.html',
+  './zitije-petar-cetinjski.html',
+  './zitije-petrovdan.html',
+  './zitije-preobrazenje.html',
+  './zitije-prorok-ilija.html',
+  './zitije-sabor.html',
+  './zitije-silazak-duha-svetoga.html',
+  './zitije-sretenje-gospodnje.html',
+  './zitije-sv-sava.html',
+  './zitije-sveta-petka.html',
+  './zitije-sveta-tri-jerarha.html',
+  './zitije-sveti-prvomucenik-stefan.html',
+  './zitije-sveti-vasilije-veliki.html',
+  './zitije-usekovanje.html',
+  './zitije-uspenije.html',
+  './zitije-vasilije-ostroski.html',
+  './zitije-vaskrs.html',
+  './zitije-vaskrsni-ponedeljak.html',
+  './zitije-vaskrsni-utorak.html',
+  './zitije-vavedenje.html',
+  './zitije-vaznesenje.html',
+  './zitije-veliki-petak.html',
+  './vidovdan.html',
+  './jovan-1.html', './jovan-2.html', './jovan-3.html', './jovan-4.html', './jovan-5.html',
+  './jovan-6.html', './jovan-7.html', './jovan-8.html', './jovan-9.html', './jovan-10.html',
+  './jovan-11.html', './jovan-12.html', './jovan-13.html', './jovan-14.html', './jovan-15.html',
+  './jovan-16.html', './jovan-17.html', './jovan-18.html', './jovan-19.html', './jovan-20.html',
+  './jovan-21.html',
+  './matej-1.html', './matej-2.html', './matej-3.html', './matej-4.html', './matej-5.html',
+  './matej-6.html', './matej-7.html', './matej-8.html', './matej-9.html', './matej-10.html',
+  './matej-11.html', './matej-12.html', './matej-13.html', './matej-14.html', './matej-15.html',
+  './matej-16.html', './matej-17.html', './matej-18.html', './matej-19.html', './matej-20.html',
+  './matej-21.html', './matej-22.html', './matej-23.html', './matej-24.html', './matej-25.html',
+  './matej-26.html', './matej-27.html', './matej-28.html',
+  './marko-1.html', './marko-2.html', './marko-3.html', './marko-4.html', './marko-5.html',
+  './marko-6.html', './marko-7.html', './marko-8.html', './marko-9.html', './marko-10.html',
+  './marko-11.html', './marko-12.html', './marko-13.html', './marko-14.html', './marko-15.html', 
+  './marko-16.html',
+  './luka-1.html', './luka-2.html', './luka-3.html', './luka-4.html', './luka-5.html',
+  './luka-6.html', './luka-7.html', './luka-8.html', './luka-9.html', './luka-10.html',
+  './luka-11.html', './luka-12.html', './luka-13.html', './luka-14.html', './luka-15.html',
+  './luka-16.html', './luka-17.html', './luka-18.html', './luka-19.html', './luka-20.html',
+  './luka-21.html', './luka-22.html', './luka-23.html', './luka-24.html',
+  './o-molitvi-sv-jovan-krostantski.html',
+  './jutarnje-molitve.html',
+  './molitveno-pravilo-serafima-sarovskog.html',
+  './bogorodice-djevo.html',
+  './simbol-vere.html',
+  './molitva-za-decu.html',
+  './molitva-za-roditelje.html',
+  './molitva-za-otvorenje-uma.html',
+  './molitva-svetog-filareta.html',
+  './velikoposna-molitva-svetog-jefrema-sirina.html',
+  './molitva-pokajnika.html',
+  './molitve-andjelu-cuvaru.html',
+  './molitva-isusu-hristu.html',
+  './molitve-svetim-arhangelima-za-svaki-dan-u-nedelji.html',
+  './molitve-presvetoj-bogorodici-za-svaki-dan-u-nedelji.html',
+  './molitva-za-spasenje-od-rasejanosti-uma.html',
+  './molitva-bogorodici.html',
+  './molitva-u-tuzi-dusevnoj.html',
+  './molitva-za-upokojene.html',
+  './molitva-za-upokojene-prestavljenih.html',
+  './molitva-za-zastitu-od-necistih-sila.html',
+  './pokajna-molitva.html',
+  './molitva-za-oprostaj-greha.html',
+  './molitva-presvetoj-bogorodici-brzopomocnici.html',
+  './molitve-za-svetu-tajnu-braku.html',
+  './molitva-presvetoj-bogorodici-u-nevolji-i-potistenosti.html',
+  './molitvе-svetom-vasiliju-ostroskom.html',
+  './molitva-protiv-hulnih-pomisli.html',
+  './zahvalna-molitva.html',
+  './molitva-za-umnozenje-ljubavi.html',
+  './molitva-za-one-koji-putuju.html',
+  './molitva-svetom-naumu-ohridskom-cudotvorcu.html',
+  './molitva-za-bolesnika.html',
+  './molitva-pred-ispovest.html',
+  './podsetnik-za-ispovest.html',
+  './molitva-pred-sveto-pricesce.html',
+  './molitva-posle-svetog-pricesca.html',
+  './molitve-pre-i-posle-ucenja.html',
+  './molitve-pre-spavanja.html',
+  './molitva-pre-uzimanja-nafore-i-svete-vode.html',
+  './hrabrost-za-molitvu.html',
+  './ucenje-starca-vasilija-o-isusovoj-molitvi.html',
   './manifest.json',
-  './ikona.png',
-  './web-app-manifest-192x192.png',
-  './web-app-manifest-512x512.png'
+  './svetitelji.json'
 ];
 
-// 1. Instalacija - sada zaključava CELU Riznicu sa svim glavnim stranicama odjednom!
+// Instalacija i keširanje
 self.addEventListener('install', evt => {
-  self.skipWaiting(); 
   evt.waitUntil(
     caches.open(cacheName).then(cache => {
-      console.log('Skladištim kompletan kostur Riznice u keš...');
+      console.log('Усисивач је спаковао све моливе и јеванђеља!');
       return cache.addAll(assets);
     })
   );
 });
 
-// 2. Aktivacija i čišćenje starih verzija
+// Aktivacija i čišćenje starih keševa
 self.addEventListener('activate', evt => {
   evt.waitUntil(
-    Promise.all([
-      self.clients.claim(),
-      caches.keys().then(keys => {
-        return Promise.all(keys
-          .filter(key => key !== cacheName)
-          .map(key => caches.delete(key))
-        );
-      })
-    ])
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+      );
+    })
   );
 });
 
-// 3. Fetch - Pametni imuni usisivač koji ignoriše Google Drive i čuva tvoj sajt
+// Fetch event - služi sve iz keša kad nema neta
 self.addEventListener('fetch', evt => {
-  if (!evt.request.url.startsWith('http')) return;
-
-  // Spas za Google Drive audio fajlove
-  if (evt.request.url.includes('google.com') || evt.request.url.includes('googleusercontent.com')) {
-    return; 
-  }
-
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
-      if (cacheRes) return cacheRes;
-
-      return fetch(evt.request).then(fetchRes => {
-        if (!fetchRes || fetchRes.status !== 200 || fetchRes.type !== 'basic') {
-          return fetchRes;
-        }
-
+      return cacheRes || fetch(evt.request).then(fetchRes => {
         return caches.open(cacheName).then(cache => {
-          cache.put(evt.request.url, fetchRes.clone());
+          // Dinamički usisava ako se pojavi neki novi fajl
+          if (evt.request.url.startsWith(self.location.origin)) {
+            cache.put(evt.request.url, fetchRes.clone());
+          }
           return fetchRes;
         });
       });
     }).catch(() => {
-      if (evt.request.url.indexOf('.html') > -1) {
-        return caches.match('./index.html');
-      }
-    })
-  );
-});
-    }).catch(() => {
+      // Ako ne nađe u kešu, a nema neta, otvara index.html
       if (evt.request.url.indexOf('.html') > -1) {
         return caches.match('./index.html');
       }
